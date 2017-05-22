13312371dae22a3a4dfb6871aaceed4cb9860a9c
Robert <robert0714@gmail.com>
2017-05-21 22:56:46 -0500

    Create seneca-web-2.js

---
+++ Chapter 02/seneca-web-2.js
@@ -0,0 +1,34 @@
+'use strict'
+
+var Seneca = require('seneca');
+var Express = require('express');
+var Web = require('seneca-web');
+
+seneca.add({role: 'api',cmd:'bagzina'}, function(args, done) {
+     done(null, {bar:"Bagzina!"});
+     });
+     
+var Routes = [{
+     prefix: '/my-api',
+     pin: 'role:api,cmd:*',
+     map: {
+          bagzina: {GET: true}
+          }
+     }];
+     
+var seneca = Seneca();
+ 
+var config = {
+     routes: Routes,
+     adapter: require('seneca-web-adapter-express'),
+     context: Express()
+};
+
+ seneca.client()
+.use(Web, config)
+.ready(() => {
+    var server = seneca.export('web/context')()
+    server.listen('8080', () => {
+        console.log('server started on: 8080')
+    })
+});
